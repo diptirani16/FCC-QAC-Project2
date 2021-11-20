12c3381e039eb3e885f4cc36c95b6f7f198ec139
Dipti Rani <gdipti642@gmail.com>
2021-11-20 19:00:53 +0530

    add post and delete requests

--- routes/api.js
+++ routes/api.js
@@ -1,5 +1,12 @@
 'use strict';
 
+const Server = require('../database/server');
+Server.connection(process.env.MONGO).then(() => {
+  console.log('Database connected!')
+}).catch((err) => {
+  console.log(err);
+})
+
 module.exports = function (app) {
 
   app.route('/api/issues/:project')
@@ -11,7 +18,30 @@ module.exports = function (app) {
     
     .post(function (req, res){
       let project = req.params.project;
-      
+      let { issue_title, issue_text, created_by, assigned_to, status_text } = req.body;
+
+      if (!issue_title || !issue_text || !created_by || issue_title === '' || issue_text === '' || created_by === '') {
+        return res.json({ error: 'required field(s) missing' });
+      }
+
+      const created_on = new Date().toISOString();
+      const updated_on = created_on;
+      const open = true;
+
+      if(!assigned_to) {
+        assigned_to = "";
+      }
+      if(!status_text) {
+        status_text = "";
+      }
+
+      Server.addIssue({issue_title, issue_text, created_on, updated_on, created_by, assigned_to, open, status_text, project})
+        .then((newData) => {
+          return res.json(newData)
+        })
+        .catch(() => {
+          return res.send("Error !!");
+        })
     })
     
     .put(function (req, res){
@@ -20,7 +50,24 @@ module.exports = function (app) {
     })
     
     .delete(function (req, res){
-      let project = req.params.project;
+      let idToDelete = req.body['_id'];
+
+      if(!idToDelete || idToDelete === ""){
+        return res.json({ error: 'missing_id'});
+      }
+
+      Server.deleteIssue(idToDelete)
+        .then(() =>
+          res.json({
+            result: 'successfully deleted',
+            '_id': idToDelete
+          }))
+        .catch(() => {
+          res.json({ 
+            error: 'could not delete',
+            '_id': idToDelete
+          })
+        })
       
     });
     
