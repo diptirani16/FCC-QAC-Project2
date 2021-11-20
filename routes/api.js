f935a89c1f203d052415307ed62be33ddd109efe
Dipti Rani <gdipti642@gmail.com>
2021-11-20 19:22:04 +0530

    add get and put requests

--- routes/api.js
+++ routes/api.js
@@ -7,13 +7,52 @@ Server.connection(process.env.MONGO).then(() => {
   console.log(err);
 })
 
+const updatedIssueObject = (data) => {
+  let output = {};
+
+  if(data['open']) {
+    output['open'] = false;
+  }
+  else {
+    output['open'] = true;
+  }
+
+  for(let property in data) {
+    if(property === 'open') {
+      continue;
+    }
+    if(data[property] !== '' && property !== '_id') {
+      output[property] = data[property];
+    }
+  }
+
+  output['updated_on'] = new Date().toISOString();
+  return output;
+}
+
 module.exports = function (app) {
 
   app.route('/api/issues/:project')
   
     .get(function (req, res){
       let project = req.params.project;
-      
+      let issueData = {};
+
+      if(Object.keys(req.query).length !== 0) {
+        issueData = {...req.query};
+      }
+      else {
+        issueData.project = project; 
+      }
+
+      Server.findIssue(issueData)
+        .then((data) => {
+        res.json(data);
+        })
+        .catch((err) => {
+          res.send('Error !!');
+        })
+
     })
     
     .post(function (req, res){
@@ -45,8 +84,34 @@ module.exports = function (app) {
     })
     
     .put(function (req, res){
-      let project = req.params.project;
+      let idToUpdate = req.body['_id'];
       
+      if(!idToUpdate || idToUpdate === "") {
+        return res.json({ error: 'missing_id' });
+      }
+
+      if(Object.keys(req.body).length <= 1) {
+        return res.json({ 
+          error: 'no update field(s) sent',
+          '_id': idToDelete
+        })
+      }
+
+      const updatedIssue = updatedIssueObject(req.body);
+
+      Server.updateIssue(idToUpdate, updatedIssue)
+        .then(() => {
+          return res.json({
+            result: 'successfully updated',
+            '_id': idToUpdate
+          })
+        })
+        .catch(() => {
+          res.json({
+            error: 'could not update',
+            '_id': idToUpdate
+          })
+        })
     })
     
     .delete(function (req, res){
